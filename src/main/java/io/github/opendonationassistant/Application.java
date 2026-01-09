5075b54d3e02301d078ba839da3d0c0401c17dc8
stCarolas <stcarolas@gmail.com>
2026-01-09 13:50:55 +0300

    add missing rabbitmq config

--- src/main/java/io/github/opendonationassistant/Application.java
+++ src/main/java/io/github/opendonationassistant/Application.java
@@ -1,11 +1,14 @@
 package io.github.opendonationassistant;
 
+import io.github.opendonationassistant.rabbit.RabbitConfiguration;
 import io.micronaut.context.ApplicationContextBuilder;
 import io.micronaut.context.ApplicationContextConfigurer;
 import io.micronaut.context.annotation.ContextConfigurer;
+import io.micronaut.rabbitmq.connect.ChannelInitializer;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.*;
 import io.swagger.v3.oas.annotations.info.*;
+import jakarta.inject.Singleton;
 
 @OpenAPIDefinition(info = @Info(title = "oda-twitch-service"))
 public class Application {
@@ -22,5 +25,10 @@ public class Application {
 
   public static void main(String[] args) {
     Micronaut.build(args).mainClass(Application.class).banner(false).start();
+  }
+
+  @Singleton
+  public ChannelInitializer rabbitConfiguration() {
+    return new RabbitConfiguration();
   }
 }
