4eae2552c0d193edb1b2f0118428ca4c9573a5dd
stCarolas <stcarolas@gmail.com>
2026-05-24 19:13:47 +0300

    fix missing uuid type for entity mapping

--- src/main/java/io/github/opendonationassistant/twitch/repository/TwitchWebhook.java
+++ src/main/java/io/github/opendonationassistant/twitch/repository/TwitchWebhook.java
@@ -13,7 +13,7 @@ public record TwitchWebhook(
   @Id @MappedProperty(value = "id", type = DataType.UUID) String id,
   @MappedProperty("recipient_id") String recipientId,
   @MappedProperty("twitch_id") String twitchId,
-  @MappedProperty("refresh_token_id") String refreshTokenId,
+  @MappedProperty(value = "refresh_token_id", type = DataType.UUID) String refreshTokenId,
   @MappedProperty(value = "subscription_ids", type = DataType.STRING_ARRAY)
   List<String> subscriptionIds
 ) {}
