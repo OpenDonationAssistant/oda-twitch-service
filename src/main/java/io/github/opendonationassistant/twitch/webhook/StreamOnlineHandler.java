426b81d6f8d2ec1a168e0f71d2fc1883f2f9dffc
stCarolas <stcarolas@gmail.com>
2026-08-07 19:20:51 +0300

    add handling missing stream online

--- src/main/java/io/github/opendonationassistant/twitch/webhook/StreamOnlineHandler.java
+++ src/main/java/io/github/opendonationassistant/twitch/webhook/StreamOnlineHandler.java
@@ -1,17 +1,20 @@
 package io.github.opendonationassistant.twitch.webhook;
 
+import io.github.opendonationassistant.commons.logging.ODALogger;
 import io.github.opendonationassistant.events.twitch.TwitchFacade;
 import io.github.opendonationassistant.events.twitch.events.TwitchStreamStartedEvent;
 import io.github.opendonationassistant.integration.twitch.TwitchApiClient;
 import io.github.opendonationassistant.integration.twitch.TwitchClient;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 @Singleton
 public class StreamOnlineHandler implements TwitchEventHandler {
 
+  private ODALogger log = new ODALogger(this);
   private final TwitchFacade facade;
   private final TwitchClient client;
 
@@ -36,16 +39,28 @@ public class StreamOnlineHandler implements TwitchEventHandler {
         "live"
       )
       .thenAccept(stream -> {
-        Optional.ofNullable(stream.data().getFirst())
+        final Optional<TwitchApiClient.Stream> first = stream
+          .data()
+          .stream()
+          .findFirst();
+        first
           .map(TwitchApiClient.Stream::thumbnailUrl)
-          .ifPresent(thumbnailUrl ->
-            facade.sendEvent(
-              new TwitchStreamStartedEvent(
-                context.id(),
-                context.account().recipientId(),
-                thumbnailUrl
-              )
-            )
+          .ifPresentOrElse(
+            thumbnailUrl -> {
+              facade.sendEvent(
+                new TwitchStreamStartedEvent(
+                  context.id(),
+                  context.account().recipientId(),
+                  thumbnailUrl
+                )
+              );
+            },
+            () -> {
+              log.error(
+                "Could find stream online",
+                Map.of("recipientId", context.account().recipientId())
+              );
+            }
           );
       });
   }
